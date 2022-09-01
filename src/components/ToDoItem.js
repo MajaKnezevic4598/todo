aa09c6eca532e57e9ee9efd1818ab8fc47b060dc
Maja Knezevic <maja.knezevic1@gmail.com>
2022-09-01 23:20:51 +0200

    added delete and update if task is finished functionality

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ToDoItem.scss";
 import { FiEdit } from "react-icons/fi";
 import { TiDelete } from "react-icons/ti";
 import { HiCheck } from "react-icons/hi";
+import Modal from "react-modal";
 
+Modal.setAppElement("#root");
 function ToDoItem({ title, id, setToDo, toDo }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   const handleChacked = () => {
     setToDo(
       toDo.map((item) => {
@@ -15,6 +19,11 @@ function ToDoItem({ title, id, setToDo, toDo }) {
 
   const { done } = toDo.find((item) => item.id === id);
 
+  const handleDelete = () => {
+    const filtered = toDo.filter((item) => item.id !== id);
+    setToDo(filtered);
+  };
+
   return (
     <div className="toDo-conteiner">
       <div className="todo-title">{title}</div>
@@ -24,10 +33,33 @@ function ToDoItem({ title, id, setToDo, toDo }) {
           {done && <HiCheck className="checked" />}{" "}
         </div>
 
-        <FiEdit className="edit" />
+        <FiEdit
+          className="edit"
+          onClick={() => {
+            setModalIsOpen(true);
+          }}
+        />
 
-        <TiDelete className="delete" />
+        <TiDelete className="delete" onClick={handleDelete} />
       </div>
+      <Modal
+        className="Modal"
+        overlayClassName="Overlay"
+        isOpen={modalIsOpen}
+        onRequestClose={() => {
+          setModalIsOpen(false);
+        }}
+      >
+        <h1>ja sam modal</h1>
+        <div
+          className="close-modal"
+          onClick={() => {
+            setModalIsOpen(false);
+          }}
+        >
+          close
+        </div>
+      </Modal>
     </div>
   );
 }
